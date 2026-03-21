5666353f0df617246a1a09662313db440462ab32
Cardone Bert <122242831+bert4lyf@users.noreply.github.com>
2026-03-21 06:15:38 +0000

    for the love of the game

--- predictor.js
+++ predictor.js
@@ -10,7 +10,7 @@ class EnsemblePredictor {
             entropyReduction: 0.20 // Signal-to-noise filtering
         };
 
-        this.TARGET_COUNT = 5; // Always output exactly 5 numbers
+        this.TARGET_COUNT = 2; // Always output exactly 5 numbers
     }
 
     /**
